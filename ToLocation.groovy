fe10940ef5d2de6fe1c56af9aae1db2146a3db32
Mladen Petr <mladenpetr94@gmail.com>
2017-05-09 00:50:15 +0200

    v1.01

--- ToLocation.groovy
+++ ToLocation.groovy
@@ -29,12 +29,11 @@ import com.maxmind.geoip.*
 
 public static void main (String[] args){
 
-	LookupService cl = new LookupService("E:\\FER\\GeoIP\\GeoLiteCity.dat",
+	LookupService ls = new LookupService("E:\\FER\\GeoIP\\GeoLiteCity.dat",
 		LookupService.GEOIP_MEMORY_CACHE ); // replace the string in this command with path to your GeoLiteCity database!
-	def location=cl.getLocation(node.getPlainText())
+	def location=ls.getLocation(node.getPlainText())
 	if (location==null) {
 		ui.errorMessage('The node text is not an IP address nor a domain name!')
-
 	}
 	else {
 		if (location.city==null){

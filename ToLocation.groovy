75387793273886efe13010e8f93cf2406f55fa38
Mladen Petr <mladenpetr94@gmail.com>
2017-05-09 00:36:56 +0200

    ToLocation script
    Added ToLocation script.

---
+++ ToLocation.groovy
@@ -0,0 +1,46 @@
+/*
+    FreeTransform, scripts that perform simple Maltego transformations.
+    Copyright (C) 2017  Mladen Petr
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+	The script is called upon an arbitrary node.
+	Parameter is node text.
+	The script works correctly on nodes that represent an IP address
+	or a domain name.
+	In case of a incorrect script call, it creates appropriate error message.
+*/
+
+import com.maxmind.geoip.*
+
+public static void main (String[] args){
+
+	LookupService cl = new LookupService("E:\\FER\\GeoIP\\GeoLiteCity.dat",
+		LookupService.GEOIP_MEMORY_CACHE );
+	def location=cl.getLocation(node.getPlainText())
+	if (location==null) {
+		ui.errorMessage('The node text is not an IP address nor a domain name!')
+
+	}
+	else {
+		if (location.city==null){
+			location.city='Unknown'
+		}
+		def child=node.createChild(location.city+","+location.countryName)
+	}
+}
+	
+

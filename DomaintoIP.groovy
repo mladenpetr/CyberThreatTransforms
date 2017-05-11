13a618d1004e6cc2d4c7bccfc1451cf331183a00
Mladen Petr <mladenpetr94@gmail.com>
2017-05-11 23:22:54 +0200

    New scripts
    Added new scripts.
    The code is not optimised.
    Template method design pattern is yet to be implemented.

---
+++ DomaintoIP.groovy
@@ -0,0 +1,40 @@
+/*
+    FreeTransform, scripts that perform simple Maltego transformations.
+    Copyright (C) 2017  Mladen Petr
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+    The script is called upon an arbitrary node.
+    Parameter is node text.
+    The script works correctly on nodes that represent an IP address.
+    In case of a incorrect script call, it creates appropriate error message.
+    The script uses java.net.InetAddress class to get relevant information.
+    v1.0 without template method
+*/
+
+public static void main (String[] args){
+
+	InetAddress[] domains=java.net.InetAddress.getAllByName(node.getPlainText())
+    if (domains==null){
+        ui.errorMessage('The node text is not a domain name!')
+    }
+    else {
+	   for (InetAddress domain:domains){
+	       def newNode=node.createChild()
+	       newNode.setText(domain.getHostAddress())
+	   }
+    }
+}
---
+++ ToTwitterUsername.groovy
@@ -0,0 +1,50 @@
+/*
+    FreeTransform, scripts that perform simple Maltego transformations.
+    Copyright (C) 2017  Mladen Petr
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+	The script is called upon an arbitrary node.
+	Parameter is node text.
+	The scripts runs a search using twitter API and returns first 20 users that are related to
+	the parameter. (It is possible to do 1000 results)
+	Results are shown with attached link to twitter profile.
+	If nothing is found, the user is properly notified.
+*/
+
+import twitter4j.*
+import twitter4j.conf.ConfigurationBuilder
+
+static void main(String[] args) {
+
+	ConfigurationBuilder cb = new ConfigurationBuilder()
+	cb.setDebugEnabled(true).setOAuthConsumerKey("CONSUMER_KEY").setOAuthConsumerSecret("CONSUMER_SECRET").setOAuthAccessToken("ACCESS_TOKEN").setOAuthAccessTokenSecret("ACCESS_TOKEN_SECRET")
+	TwitterFactory tf = new TwitterFactory(cb.build())
+	Twitter twitter = tf.getInstance()
+	ResponseList<User> users = twitter.searchUsers(node.getPlainText(),1)
+	if (users.size()==0){
+		ui.informationMessage("Search didn't return any result.")
+	}
+	else {
+		for (User user: users){
+			def child = node.createChild()
+	    	child.setText("@"+user.getScreenName()+","+user.getName())
+	    	child.link.text='https://twitter.com/'+user.getScreenName()
+		}
+	}
+}
+
+

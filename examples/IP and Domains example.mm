b73c953c4dcb99a8954d69752009c1c5198f47b9
Mladen Petr <mladenpetr94@gmail.com>
2017-05-20 22:23:31 +0200

    Examples
    Added freeplane examples

---
+++ examples/IP and Domains example.mm
@@ -0,0 +1,114 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="IP and Domains example" FOLDED="false" ID="ID_1869939514" CREATED="1495310291731" MODIFIED="1495310525585" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="www.fer.unizg.hr" POSITION="right" ID="ID_439908121" CREATED="1495310309827" MODIFIED="1495310359507" HGAP_QUANTITY="13.250000022351742 pt" VSHIFT_QUANTITY="-71.99999785423284 pt">
+<edge COLOR="#0000ff"/>
+<node TEXT="161.53.72.120" ID="ID_312459675" CREATED="1495310356778" MODIFIED="1495310356785">
+<node TEXT="Zagreb,Croatia" ID="ID_1652335045" CREATED="1495310381021" MODIFIED="1495310381023"/>
+</node>
+</node>
+<node TEXT="www.freeplane.org" POSITION="left" ID="ID_1483297817" CREATED="1495310319239" MODIFIED="1495310393107" HGAP_QUANTITY="-361.7499888017777 pt" VSHIFT_QUANTITY="55.49999834597116 pt">
+<edge COLOR="#00ff00"/>
+<node TEXT="216.34.181.153" ID="ID_946889253" CREATED="1495310384608" MODIFIED="1495310399437" HGAP_QUANTITY="-184.7499940767886 pt" VSHIFT_QUANTITY="4.499999865889553 pt">
+<node TEXT="Chesterfield,United States" ID="ID_497302330" CREATED="1495310396455" MODIFIED="1495310399436" HGAP_QUANTITY="-224.4999928921463 pt" VSHIFT_QUANTITY="1.4999999552965178 pt"/>
+</node>
+</node>
+<node TEXT="github.com" POSITION="right" ID="ID_269119010" CREATED="1495310340851" MODIFIED="1495310361914" HGAP_QUANTITY="37.999999284744284 pt" VSHIFT_QUANTITY="-16.499999508261695 pt">
+<edge COLOR="#ff00ff"/>
+<node TEXT="192.30.253.113" ID="ID_764292833" CREATED="1495310369764" MODIFIED="1495310369767">
+<node TEXT="San Francisco,United States" ID="ID_1750892014" CREATED="1495310377208" MODIFIED="1495310377220"/>
+</node>
+<node TEXT="192.30.253.112" ID="ID_882299467" CREATED="1495310369768" MODIFIED="1495310369769"/>
+</node>
+<node TEXT="www.youtube.com" POSITION="left" ID="ID_1199113709" CREATED="1495310402240" MODIFIED="1495310483584" HGAP_QUANTITY="-339.24998947232996 pt" VSHIFT_QUANTITY="68.99999794363981 pt">
+<edge COLOR="#00ffff"/>
+<node TEXT="216.58.208.46" ID="ID_797249687" CREATED="1495310444167" MODIFIED="1495310477503" HGAP_QUANTITY="-168.24999456852692 pt" VSHIFT_QUANTITY="15.749999530613437 pt">
+<node TEXT="Mountain View,United States" ID="ID_989130702" CREATED="1495310447361" MODIFIED="1495310477502" HGAP_QUANTITY="-228.9999927580359 pt" VSHIFT_QUANTITY="11.24999966472388 pt"/>
+</node>
+</node>
+<node TEXT="notawebsite.error" POSITION="right" ID="ID_1968764455" CREATED="1495310490727" MODIFIED="1495310757287" HGAP_QUANTITY="-453.24998607486526 pt" VSHIFT_QUANTITY="11.999999642372142 pt">
+<edge COLOR="#7c0000"/>
+</node>
+</node>
+</map>
---
+++ examples/Twitter example.mm
@@ -0,0 +1,120 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Twitter example" FOLDED="false" ID="ID_936079707" CREATED="1495310941309" MODIFIED="1495311495496" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Freeplane" POSITION="right" ID="ID_691021171" CREATED="1495310950379" MODIFIED="1495310964724">
+<edge COLOR="#ff0000"/>
+<node TEXT="@freeplane,Freeplane Team" ID="ID_1185982243" CREATED="1495310970838" MODIFIED="1495310971150" LINK="https://twitter.com/freeplane"/>
+<node TEXT="@hiro_freeplane,&#x76f8;&#x4e92;&#xff20;&#x30d2;&#x30ed;&#x30a2;&#x30ad;" ID="ID_1789237662" CREATED="1495310971161" MODIFIED="1495310971168" LINK="https://twitter.com/hiro_freeplane"/>
+<node TEXT="@FreeplaneNews,John Harvey" ID="ID_1095022808" CREATED="1495310971168" MODIFIED="1495310971169" LINK="https://twitter.com/FreeplaneNews"/>
+</node>
+<node TEXT="Mladen Petr" POSITION="left" ID="ID_206348146" CREATED="1495311028791" MODIFIED="1495311033512">
+<edge COLOR="#0000ff"/>
+<node TEXT="@mlaggi0,Mladen Petr" ID="ID_1772201004" CREATED="1495311036883" MODIFIED="1495311036884" LINK="https://twitter.com/mlaggi0">
+<node TEXT="@LisaVanderpump,Lisa Vanderpump" ID="ID_1992279737" CREATED="1495311084253" MODIFIED="1495311084255" LINK="https://twitter.com/LisaVanderpump"/>
+<node TEXT="@RealHughJackman,Hugh Jackman" ID="ID_1347533439" CREATED="1495311084255" MODIFIED="1495311084255" LINK="https://twitter.com/RealHughJackman"/>
+<node TEXT="@jimmykimmel,Jimmy Kimmel" ID="ID_438124322" CREATED="1495311084256" MODIFIED="1495311084256" LINK="https://twitter.com/jimmykimmel"/>
+<node TEXT="@ericschmidt,Eric Schmidt" ID="ID_536205137" CREATED="1495311084256" MODIFIED="1495311084256" LINK="https://twitter.com/ericschmidt"/>
+<node TEXT="@CuteOverloads,Cute Overloads" ID="ID_362989104" CREATED="1495311084256" MODIFIED="1495311084257" LINK="https://twitter.com/CuteOverloads"/>
+<node TEXT="@RichardDawkins,Richard Dawkins" ID="ID_125956982" CREATED="1495311084257" MODIFIED="1495311084257" LINK="https://twitter.com/RichardDawkins"/>
+<node TEXT="@KolindaGK,Kolinda GK" ID="ID_1184309900" CREATED="1495311084257" MODIFIED="1495311084258" LINK="https://twitter.com/KolindaGK"/>
+<node TEXT="@Naughty_Dog,Naughty Dog" ID="ID_92765826" CREATED="1495311084258" MODIFIED="1495311084258" LINK="https://twitter.com/Naughty_Dog"/>
+<node TEXT="@Pokemon,Pok&#xe9;mon" ID="ID_793837504" CREATED="1495311084259" MODIFIED="1495311084259" LINK="https://twitter.com/Pokemon"/>
+<node TEXT="@MarioMandzukic9,Mario Mand&#x17e;uki&#x107; MM17" ID="ID_712358614" CREATED="1495311084259" MODIFIED="1495311084260" LINK="https://twitter.com/MarioMandzukic9"/>
+<node TEXT="@sethmeyers,Seth Meyers" ID="ID_1001025733" CREATED="1495311084260" MODIFIED="1495311084260" LINK="https://twitter.com/sethmeyers"/>
+<node TEXT="@AndreaRussett,Andrea Russett" ID="ID_810422298" CREATED="1495311084261" MODIFIED="1495311084262" LINK="https://twitter.com/AndreaRussett"/>
+<node TEXT="@GarethBale11,Gareth Bale" ID="ID_1049696392" CREATED="1495311084262" MODIFIED="1495311084263" LINK="https://twitter.com/GarethBale11"/>
+<node TEXT="@danielhowell,Daniel Howell" ID="ID_1731257605" CREATED="1495311084263" MODIFIED="1495311084264" LINK="https://twitter.com/danielhowell"/>
+<node TEXT="@CuteEmergency,Cute Emergency" ID="ID_935451616" CREATED="1495311084264" MODIFIED="1495311084264" LINK="https://twitter.com/CuteEmergency"/>
+</node>
+</node>
+<node TEXT="randomsearch" POSITION="right" ID="ID_1759473300" CREATED="1495311327244" MODIFIED="1495311447790" HGAP_QUANTITY="17.749999888241295 pt" VSHIFT_QUANTITY="62.999998122453746 pt">
+<edge COLOR="#ff00ff"/>
+<node TEXT="@Randomsearch201,Random" ID="ID_1997697614" CREATED="1495311336220" MODIFIED="1495311336222" LINK="https://twitter.com/Randomsearch201"/>
+<node TEXT="@hkkcp1,randomsearch" ID="ID_1416062781" CREATED="1495311336223" MODIFIED="1495311336223" LINK="https://twitter.com/hkkcp1"/>
+</node>
+<node TEXT="this_search_will_not_find_anyone" POSITION="left" ID="ID_1045338631" CREATED="1495311472993" MODIFIED="1495311508193" HGAP_QUANTITY="-25.74999881535772 pt" VSHIFT_QUANTITY="26.99999919533732 pt">
+<edge COLOR="#00ffff"/>
+</node>
+</node>
+</map>
